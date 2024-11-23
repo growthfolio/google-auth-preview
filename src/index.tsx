cce4f62aa4720db19196a389d2b1603d8a51db2d
Felipe.Macedo <felipealexandrej@gmail.com>
2024-11-23 17:23:38 -0300

    fix: update firebase hosting workflow

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
@@ -7,14 +7,17 @@ import App from "./App";
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 if (!clientId) {
-  throw new Error("Google Client ID não está definido!");
+  console.error("REACT_APP_GOOGLE_CLIENT_ID não foi configurado corretamente!");
+  throw new Error("Google Client ID não está disponível.");
 }
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <GoogleOAuthProvider clientId={clientId}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </GoogleOAuthProvider>,
-  document.getElementById("root")
+  </GoogleOAuthProvider>
 );
